0efc9168ae7c7550f7d80e2d211e2fdc1f4a9290
Jesper Fischer Ehmsen <au645332@uni.au.dk>
2023-01-23 10:05:32 +0100

    updates

--- scripts.R
+++ scripts.R
@@ -134,3 +134,99 @@ get_data1 = function(){
   return(data)
   
 }
+
+
+
+
+get_summary = function(data){
+  table = data %>% 
+    filter(gruppe == 1) %>% 
+    group_by(tid) %>% 
+    summarize(n = n(),
+              crimpright = round(mean(crimphøjremax),0), 
+              ch_sd = round(sd(crimphøjremax)/sqrt(n),1), 
+              crimpleft = round(mean(crimpvenstremax, na.rm =T),0), 
+              cv_sd = round(sd(crimpvenstremax, na.rm =T)/sqrt(n),1),
+              lattice = round(mean(lattice_r, na.rm = T),0),
+              lattice_sd = round(sd(lattice_r, na.rm = T)/sqrt(n),1),
+              lockoffleft = round(mean(ll_r, na.rm = T),0),
+              ll_sd = round(sd(ll_r, na.rm = T)/sqrt(n),1),
+              lockoffright = round(mean(lr_r, na.rm = T),0),
+              lr_sd = round(sd(lr_r, na.rm = T)/sqrt(n),1),
+              moontal = round(mean(moon.tal, na.rm = T),0),
+              moontal_sd = round(sd(moon.tal, na.rm = T)/sqrt(n),1),
+              Bw = round(mean(BW, na.rm = T),0),
+              BW_sd = round(sd(BW, na.rm = T)/sqrt(n),1),
+              pullups = round(mean(pullups_r, na.rm = T),0),
+              pullups_sd = round(sd(pullups_r, na.rm = T)/sqrt(n),1),
+              pinchhøjre = round(mean(pinchhøjremax, na.rm = T),0),
+              pinchhøjre_sd = round(sd(pinchhøjremax, na.rm = T)/sqrt(n),1),
+              pinchvenstre = round(mean(pinchvenstremax, na.rm = T),0),
+              pinchvenstre_sd = round(sd(pinchvenstremax, na.rm = T)/sqrt(n),1)
+              
+              )
+  
+
+  ncol = ncol(table)
+  for (i in 1:as.integer((ncol-2)/2)){
+    for (ii in 1:nrow(table)){
+      table[ii,ncol+i] = paste(table[ii,i*2+1],"\u00B1",table[ii,i*2+2])
+    }}
+  names = names(table)
+  realnames = names[seq(3,ncol, by = 2)]
+  names(table)[(ncol+1):ncol(table)] = realnames
+  table = table[,c(1,(ncol+1):ncol(table))]
+  
+  
+  
+  table2 = data %>% 
+    filter(gruppe == 2) %>% 
+    group_by(tid) %>% 
+    summarize(n = n(),
+              crimpright = round(mean(crimphøjremax),0), 
+              ch_sd = round(sd(crimphøjremax)/sqrt(n),1), 
+              crimpleft = round(mean(crimpvenstremax, na.rm =T),0), 
+              cv_sd = round(sd(crimpvenstremax, na.rm =T)/sqrt(n),1),
+              lattice = round(mean(lattice_r, na.rm = T),0),
+              lattice_sd = round(sd(lattice_r, na.rm = T)/sqrt(n),1),
+              lockoffleft = round(mean(ll_r, na.rm = T),0),
+              ll_sd = round(sd(ll_r, na.rm = T)/sqrt(n),1),
+              lockoffright = round(mean(lr_r, na.rm = T),0),
+              lr_sd = round(sd(lr_r, na.rm = T)/sqrt(n),1),
+              moontal = round(mean(moon.tal, na.rm = T),0),
+              moontal_sd = round(sd(moon.tal, na.rm = T)/sqrt(n),1),
+              Bw = round(mean(BW, na.rm = T),0),
+              BW_sd = round(sd(BW, na.rm = T)/sqrt(n),1),
+              pullups = round(mean(pullups_r, na.rm = T),0),
+              pullups_sd = round(sd(pullups_r, na.rm = T)/sqrt(n),1),
+              pinchhøjre = round(mean(pinchhøjremax, na.rm = T),0),
+              pinchhøjre_sd = round(sd(pinchhøjremax, na.rm = T)/sqrt(n),1),
+              pinchvenstre = round(mean(pinchvenstremax, na.rm = T),0),
+              pinchvenstre_sd = round(sd(pinchvenstremax, na.rm = T)/sqrt(n),1))
+  
+  
+  ncol = ncol(table2)
+  for (i in 1:as.integer((ncol-2)/2)){
+    for (ii in 1:nrow(table2)){
+      table2[ii,ncol+i] = paste(table2[ii,i*2+1],"\u00B1",table2[ii,i*2+2])
+    }}
+  names = names(table2)
+  realnames = names[seq(3,ncol, by = 2)]
+  names(table2)[(ncol+1):ncol(table2)] = realnames
+  table2 = table2[,c(1,(ncol+1):ncol(table2))]
+ 
+  
+  table = rbind(table,table2)
+  
+  table$tid = as.factor(table$tid)
+  levels(table$tid) = c("pre","post")
+  
+  table = as.data.frame(t(table))
+  
+     
+  
+  table = table %>%
+    row_to_names(row_number = 1)
+    
+  return(table)
+}
